df9707a22d970c076b504238bcc979f6e68c0848
Anna-Sosiura <132984503+Anna-Sosiura@users.noreply.github.com>
2024-01-01 14:36:51 +0200

    commit1

---
+++ ts/src/index.ts
@@ -0,0 +1,22 @@
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// add(1, 1);
+
+// let total: number = 100;
+// let name: string = "Bob";
+// let isActive: boolean = false;
+
+// // name = 5;
+
+import { concatenation } from "./concatenation";
+
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
+
+if (button && input) {
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
+}
